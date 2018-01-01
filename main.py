286de25637ab17762e18b9f6dc5c9418c36dc3d8
Youngbok Yoon <master@bluehack.net>
2018-01-01 21:15:38 +0900

    [BL-276]Guarantees all of Products are to be processed

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from multiprocessing import Process
 import time
 from bluelens_spawning_pool import spawning_pool
 from stylelens_product.products import Products
+from stylelens_product.crawls import Crawls
 import redis
 import pickle
 
@@ -22,6 +23,8 @@ DB_PRODUCT_NAME = os.environ['DB_PRODUCT_NAME']
 AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY'].replace('"', '')
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY'].replace('"', '')
 
+MAX_PROCESS_NUM = int(os.environ['MAX_PROCESS_NUM'])
+
 REDIS_HOST_CLASSIFY_QUEUE = 'bl:host:classify:queue'
 REDIS_PRODUCT_IMAGE_PROCESS_QUEUE = 'bl:product:image:process:queue'
 REDIS_CRAWL_VERSION = 'bl:crawl:version'
@@ -37,8 +40,11 @@ options = {
 log = Logging(options, tag='bl-image-process')
 rconn = redis.StrictRedis(REDIS_SERVER, port=6379, password=REDIS_PASSWORD)
 
-def get_latest_crawl_version():
+def get_latest_crawl_version(rconn):
   value = rconn.hget(REDIS_CRAWL_VERSION, REDIS_CRAWL_VERSION_LATEST)
+  if value is None:
+    return None
+
   log.debug(value)
   try:
     version_id = value.decode("utf-8")
@@ -56,6 +62,8 @@ def cleanup_products(host_code, version_id):
   except Exception as e:
     log.error(e)
 
+def clear_product_queue(rconn):
+  rconn.delete(REDIS_PRODUCT_IMAGE_PROCESS_QUEUE)
 
 def push_product_to_queue(product):
   rconn.lpush(REDIS_PRODUCT_IMAGE_PROCESS_QUEUE, pickle.dumps(product))
@@ -81,16 +89,12 @@ def query(host_code, version_id):
         break
       else:
         q_offset = q_offset + q_limit
-        if spawn_counter < SPAWNING_CRITERIA:
-          spawn_image_processor(str(uuid.uuid4()))
-        spawn_counter = spawn_counter + 1
 
-      time.sleep(60)
   except Exception as e:
     log.error(str(e) + ':' + host_code)
 
 
-def spawn_image_processor(uuid):
+def spawn(uuid):
   log.debug('RELEASE_MODE:' + RELEASE_MODE)
 
   pool = spawning_pool.SpawningPool()
@@ -114,6 +118,7 @@ def spawn_image_processor(uuid):
   pool.addContainerEnv(container, 'REDIS_SERVER', REDIS_SERVER)
   pool.addContainerEnv(container, 'REDIS_PASSWORD', REDIS_PASSWORD)
   pool.addContainerEnv(container, 'SPAWN_ID', uuid)
+  pool.addContainerEnv(container, 'MAX_PROCESS_NUM', str(MAX_PROCESS_NUM))
   pool.addContainerEnv(container, 'RELEASE_MODE', RELEASE_MODE)
   pool.addContainerEnv(container, 'DB_PRODUCT_HOST', DB_PRODUCT_HOST)
   pool.addContainerEnv(container, 'DB_PRODUCT_PORT', DB_PRODUCT_PORT)
@@ -126,15 +131,88 @@ def spawn_image_processor(uuid):
   pool.setRestartPolicy('Never')
   pool.spawn()
 
-def dispatch_query_job(rconn):
+def dispatch(rconn, version_id):
   global product_api
+
+  size = rconn.llen(REDIS_PRODUCT_IMAGE_PROCESS_QUEUE)
+
+  if size < MAX_PROCESS_NUM:
+    for i in range(10):
+      spawn(str(uuid.uuid4()))
+    time.sleep(60*60*2)
+
+  if size >= MAX_PROCESS_NUM and size < MAX_PROCESS_NUM * 10:
+    for i in range(100):
+      spawn(str(uuid.uuid4()))
+    time.sleep(60*60*5)
+
+  elif size >= MAX_PROCESS_NUM * 100:
+    for i in range(300):
+      spawn(str(uuid.uuid4()))
+    time.sleep(60*60*10)
+
+def prepare_products(rconn, version_id):
+  global product_api
+  offset = 0
+  limit = 200
+
+  clear_product_queue(rconn)
+  try:
+    while True:
+
+      res = product_api.get_products_by_version_id(version_id=version_id,
+                                                   is_processed=False,
+                                                   offset=offset,
+                                                   limit=limit)
+
+      log.debug("Got " + str(len(res)) + ' products')
+      for product in res:
+        push_product_to_queue(product)
+
+      if len(res) == 0:
+        break
+      else:
+        offset = offset + limit
+
+  except Exception as e:
+    log.error(str(e))
+
+def check_condition_to_start(version_id):
+  global product_api
+
   product_api = Products()
+  crawl_api = Crawls()
+
+  try:
+    # Check Crawling process is done
+    total_crawl_size = crawl_api.get_size_crawls(version_id)
+    crawled_size = crawl_api.get_size_crawls(version_id, status='done')
+    if total_crawl_size != crawled_size:
+      return False
+
+    # Check Image processing queue is empty
+    queue_size = rconn.llen(REDIS_PRODUCT_IMAGE_PROCESS_QUEUE)
+    if queue_size != 0:
+      return False
+
+  except Exception as e:
+    log.error(str(e))
+
+  return True
+
+def start(rconn):
   while True:
-    key, value = rconn.blpop([REDIS_HOST_CLASSIFY_QUEUE])
-    version_id = get_latest_crawl_version()
+    version_id = get_latest_crawl_version(rconn)
     if version_id is not None:
-      query(value.decode('utf-8'), version_id)
+      ok = check_condition_to_start(version_id)
+      if ok is True:
+        prepare_products(rconn, version_id)
+        dispatch(rconn, version_id)
+    time.sleep(60*10)
 
 if __name__ == '__main__':
-  log.info('Start bl-image-process:3')
-  Process(target=dispatch_query_job, args=(rconn,)).start()
+  log.info('Start bl-image-process:1')
+  try:
+    Process(target=start, args=(rconn,)).start()
+  except Exception as e:
+    log.error(str(e))
